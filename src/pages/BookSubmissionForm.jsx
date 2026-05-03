3721c9ca4533933c716e2036d6f05fd5ae3c4f79
Adeyemi Pelumi <adeyemipelumi1980@gmail.com>
2026-05-03 10:40:18 +0100

    Fix: Accept all common image formats (HEIC, TIFF, BMP, etc.)

--- src/pages/BookSubmissionForm.jsx
+++ src/pages/BookSubmissionForm.jsx
@@ -284,7 +284,7 @@ export default function BookSubmissionForm() {
     const handler = window.PaystackPop.setup({
       key: PAYSTACK_PUBLIC_KEY,
       email: formData.email,
-      amount: 2000000,
+      amount: 1000,
       currency: "NGN",
       ref: "TALA_" + Math.floor(Math.random() * 1000000000 + 1),
       callback: function (response) {
